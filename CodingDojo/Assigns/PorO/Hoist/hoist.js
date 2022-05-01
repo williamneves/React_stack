521050210f0db6863395a39ec287235dc13e37d2
gwilliamnn <97652695+gwilliamnn@users.noreply.github.com>
2022-05-01 13:49:50 -0400

    Hoist

---
+++ CodingDojo/Assigns/PorO/Hoist/hoist.js
@@ -0,0 +1,80 @@
+// 1
+var hello = "world";
+console.log(hello);
+
+// 2
+function test(){
+    var needle = 'magnet';
+    console.log(needle);
+}
+var needle = 'haystack';
+test();
+
+// 3
+var brendan = 'super cool';
+function print(){
+    brendan = 'only okay';
+    console.log(brendan);
+}
+console.log(brendan);
+
+// 4
+var food = "chicken";
+console.log(food);
+eat();
+function eat() {
+    food = "half-chicken";
+    console.log(food);
+    var food = "gone";
+}
+
+// 5
+mean();
+console.log(food);
+var mean = function () {
+    food = "chicken";
+    console.log(food);
+    var food = "fish";
+    console.log(food);
+}
+console.log(food);
+
+
+// 6
+mean();
+console.log(food);
+var mean = function () {
+    food = "chicken";
+    console.log(food);
+    var food = "fish";
+    console.log(food);
+}
+console.log(food);
+
+
+// 7
+mean();
+console.log(food);
+var mean = function () {
+    food = "chicken";
+    console.log(food);
+    var food = "fish";
+    console.log(food);
+}
+console.log(food);
+
+
+
+// 8
+mean();
+console.log(food);
+var mean = function () {
+    food = "chicken";
+    console.log(food);
+    var food = "fish";
+    console.log(food);
+}
+console.log(food);
+
+
+
