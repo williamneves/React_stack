bbda3621086b4ce81d64a43c2f25b5079e8801a8
gwilliamnn <97652695+gwilliamnn@users.noreply.github.com>
2022-05-01 22:33:01 -0400

    howlong

---
+++ CodingDojo/Assigns/Optional/howlong/howlong.js
@@ -0,0 +1,70 @@
+// Number.prototype.isPrime = function() {
+//     for( let i=2; i<this; i++ ) {
+//         if( this % i === 0 ) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+// Number.prototype.isPrime = function() {
+//     for(let i = 2, s = Math.sqrt(this); i <= s; i++)
+//         if(this % i === 0) return false; 
+//     return this > 1;
+// }
+
+
+// const { performance } = require('perf_hooks');
+// const start = performance.now();
+// let primeCount = 0;
+// let num = 2; // for math reasons, 1 is considered prime
+// while( primeCount < 1e4 ) {
+//     if( num.isPrime() ) {
+//         primeCount++;
+//     }
+//     num++;
+// }
+// console.log(`The 10,000th prime number is ${num-1}`);
+// console.log(`This took ${performance.now() - start} milliseconds to run`);
+
+// recursive
+// function rFib( n ) {
+//     if ( n < 2 ) {
+//         return n;
+//     }
+//     return rFib( n-1 ) + rFib( n-2 );
+// }
+// console.log('Recursive')
+// rFib(40);
+// // iterative
+// function iFib( n ) {
+//     const vals = [ 0, 1 ];
+//     while(vals.length-1 < n) {
+//         let len = vals.length;
+//         vals.push( vals[len-1] + vals[len-2] );
+//     }
+//     return vals[n];
+// }
+// console.log('Iterative')
+// iFib(40);
+
+///
+
+const { performance } = require('perf_hooks');
+const start = performance.now();
+
+const story = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident culpa nihil repellat nulla laboriosam maxime, quia aliquam ipsam reprehenderit delectus reiciendis molestias assumenda aut fugit tempore laudantium tempora aspernatur? Repellendus consequatur expedita doloribus soluta cupiditate quae fugit! Aliquid, repellat animi, illum molestias maiores, laboriosam vero impedit iusto mollitia optio labore asperiores!";
+const reversed1 = story.split("").reverse().join("");
+console.log(reversed1);
+console.log(`This took ${performance.now() - start} milliseconds to run`);
+
+// function reverseStr (str){
+//     let reversed = "";
+//     for (let i = str.length-1; i >= 0; i--) {
+//         reversed += str[i];
+//     }
+//     return reversed;
+// }
+
+// const revered2 = reverseStr(story);
+// console.log(revered2);
+// console.log(`This took ${performance.now() - start} milliseconds to run`);
