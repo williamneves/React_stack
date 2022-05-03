8f7c62ba10a39f93b3ab4e6e57a34bfb2a0e91fe
William Neves <drwilliamneves@gmail.com>
2022-05-03 16:44:40 -0400

    Assign

--- CodingDojo/Assigns/Core/PropItUp/prop-it-up/src/App.js
+++ CodingDojo/Assigns/Core/PropItUp/prop-it-up/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 import PersonCard from './components/PersonCard';
@@ -6,29 +5,16 @@ import PersonCard from './components/PersonCard';
 function App() {
   return (
     <div>
-      
         <PersonCard data={{
           firstName:`Doe`,
           lastName:`Jane`,
-          age:`45`,
+          age:45,
           hairColor:`Black`,
           }}/>
         <PersonCard data={{
           firstName:`John`,
           lastName:`Smith`,
-          age:`88`,
-          hairColor:`Brown`,
-          }}/>
-        <PersonCard data={{
-          firstName:`Millard`,
-          lastName:`Fillmore`,
-          age:`50`,
-          hairColor:`Brown`,
-          }}/>
-        <PersonCard data={{
-          firstName:`Maria`,
-          lastName:`Smith`,
-          age:`62`,
+          age:88,
           hairColor:`Brown`,
           }}/>
       
--- CodingDojo/Assigns/Core/PropItUp/prop-it-up/src/components/PersonCard.js
+++ CodingDojo/Assigns/Core/PropItUp/prop-it-up/src/components/PersonCard.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
 
 class PersonCard extends Component {
-    render() {
-        const { firstName, lastName, age, hairColor } = this.props.data;
-        return(
-            <div>
-                <h4>{lastName}, { firstName }</h4>
-                <p>Age: {age}</p>
-                <p>Hair Color: {hairColor}</p>
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: this.props.data.age,
+        };
+    }
+
+        render() {
+            return(
+                <div style={{marginLeft:30}}>
+                <h4>{this.props.data.firstName}, { this.props.data.firstName }</h4>
+                <p>Age: {this.state.age}</p>
+                <p>Hair Color: {this.props.data.hairColor}</p>
+                <input id='inputage' type='number' defaultValue={1}  min={1} max={5}/>
+                <button onClick={ () => this.incrementAge() }>Increment Age</button>
             </div>
         );
+    }
+    incrementAge = () => {
+        let amount = parseInt(document.getElementById('inputage').value);
+        this.setState({age: this.state.age + amount});
     }
 }
 
