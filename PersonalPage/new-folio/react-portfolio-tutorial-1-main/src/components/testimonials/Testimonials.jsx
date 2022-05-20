0c3d8a27fb4df9830851267a6cd59f37d27de585
William Neves <drwilliamneves@gmail.com>
2022-05-20 12:34:02 -0400

    adjusts in test

--- PersonalPage/new-folio/react-portfolio-tutorial-1-main/src/components/testimonials/Testimonials.jsx
+++ PersonalPage/new-folio/react-portfolio-tutorial-1-main/src/components/testimonials/Testimonials.jsx
@@ -21,22 +21,26 @@ const data = [
   {
     avatar: Shane,
     name: 'Shane Hummus',
+    whois: 'Youtuber, Career Espesialist',
     review: `William helped me with a complicated project involving a quiz that helps identify careers that would be a good fit for your personality and I was really impressed with his background, leadership ability, and also how fast he picked up new information.
     He also has a positive vibe and a can-do personality, I would recommend him to anyone thinking of hiring a software dev.`
   },
   {
     avatar: AVTR2,
     name: 'Shatta Wale',
+    whois: 'CEO, Shmee',
     review: 'Modi alias animi dolorem aliquam ea eum beatae maiores, consectetur praesentium quibusdam, commodi velit porro blanditiis consequatur qui molestiae. Dolorem, perspiciatis aspernatur labore distinctio ratione delectus voluptatem dolores deserunt explicabo nostrum ducimus quasi?'
   },
   {
     avatar: AVTR3,
     name: 'Kwame Despite',
+    whois: 'CEO, Shmee',
     review: 'Modi alias animi dolorem aliquam ea eum beatae maiores, consectetur praesentium quibusdam, commodi velit porro blanditiis consequatur qui molestiae. Dolorem, perspiciatis aspernatur labore distinctio ratione delectus voluptatem dolores deserunt explicabo nostrum ducimus quasi?'
   },
   {
     avatar: AVTR4,
     name: 'Nana Ama McBrown',
+    whois: 'CEO, Shmee',
     review: 'Modi alias animi dolorem aliquam ea eum beatae maiores, consectetur praesentium quibusdam, commodi velit porro blanditiis consequatur qui molestiae. Dolorem, perspiciatis aspernatur labore distinctio ratione delectus voluptatem dolores deserunt explicabo nostrum ducimus quasi?'
   },
 ]
@@ -54,13 +58,14 @@ const Testimonials = () => {
       slidesPerView={1}
       pagination={{ clickable: true }}>
         {
-          data.map(({avatar, name, review}, index) => {
+          data.map(({avatar, name, review, whois}, index) => {
             return (
               <SwiperSlide key={index} className="testimonial">
               <div className="client__avatar">
                 <img src={avatar}/>
               </div>
-              <h5 className='client__name'>{name}</h5>
+              <h4 className='client__name'>{name}</h4>
+              <h5 className='client__whois'>{whois}</h5>
               <small className='client__review'>{review}</small>
             </SwiperSlide>
             )
